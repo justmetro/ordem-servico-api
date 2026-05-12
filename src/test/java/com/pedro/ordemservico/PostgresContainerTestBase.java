2b731a2b6cb45723074de858d5b87fd650068242
Pedro Oliveira <oliveiraggpedro@gmail.com>
2026-05-11 22:36:01 -0300

    chore: add environment profiles

--- src/test/java/com/pedro/ordemservico/PostgresContainerTestBase.java
+++ src/test/java/com/pedro/ordemservico/PostgresContainerTestBase.java
@@ -1,11 +1,13 @@
 package com.pedro.ordemservico;
 
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+@ActiveProfiles("test")
 @Testcontainers
 abstract class PostgresContainerTestBase {
 
